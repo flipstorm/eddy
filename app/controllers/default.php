3510d7f7a0a6f3f36b285b7833b3f5f9eab16eee
simonhamp <simon@flipstorm.co.uk>
2011-05-10 08:34:35 +0100

    First version of EddyCore
    EddyCore is a simple branch that tracks the current master version of the 'core' folder. This makes it easier to keep the core files up to date in your apps

--- app/controllers/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace Controllers;
-	
-	class Default_Controller extends \EddyController {
-		public function index() {
-			$this->data[ 'title' ] = 'Welcome to Eddy!';
-		}
-	}
--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-	require_once '../core/init.php';
-	Eddy::init( '../app/' );
-?>
