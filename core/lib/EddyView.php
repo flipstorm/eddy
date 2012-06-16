35986cde68e2b6cc911463d91e027e7e3fdc1592
Simon Hamp <simon@flipstorm.co.uk>
2012-06-16 23:32:24 +0100

    Atomic test for the various view file's existence

--- core/lib/EddyView.php
+++ core/lib/EddyView.php
@@ -1,7 +1,8 @@
 <?php
 	class EddyView extends EddyBase {
 		public static $data;
-
+		
+		// Partials
 		public static function load_partial( $path, $data = null ) {
 			if ( is_array( $data ) ) {
 				extract( $data );
@@ -9,18 +10,23 @@
 			elseif ( is_array( self::$data ) ) {
 				extract( self::$data );
 			}
-
+			
 			$partial_path = 'views/partials/' . $path . '.part.phtml';
-			$partial_file = APP_ROOT . '/' . $partial_path;
-
-			if ( file_exists( $partial_file ) ) {
+			
+			if ( self::partial_exists( $path ) ) {
 				include( $partial_path );
 			}
 			elseif ( DEBUG ) {
-				echo "{Partial doesn't exist: $partial_file}";
+				echo "{Partial doesn't exist: $partial_path}";
 			}
 		}
+	
+		public static function partial_exists( $path ) {
+			return file_exists( APP_ROOT . '/views/partials/' . $path . '.part.phtml' );
+		}
 
+
+		// Templates
 		public static function load_template( $template, $data = null ) {
 			if ( is_array( $data ) ) {
 				extract( $data );
@@ -30,16 +36,21 @@
 			}
 			
 			$template_path = 'views/templates/' . $template . '.tmpl.phtml';
-			$template_file = APP_ROOT . '/' . $template_path;
-
-			if ( file_exists( $template_file ) ) {
+			
+			if ( self::template_exists( $template ) ) {
 				include( $template_path );
 			}
 			elseif ( DEBUG ) {
-				echo "{Template doesn't exist: $template}";
+				echo "{Template doesn't exist: $template_path}";
 			}
 		}
 
+		public static function template_exists( $path ) {
+			return file_exists( APP_ROOT . '/views/templates/' . $path . '.tmpl.phtml' );
+		}
+		
+		
+		// Main Views
 		public static function load( $view = null, $data = null ) {
 			if ( is_array( $data ) ) {
 				extract( $data );
@@ -53,13 +64,16 @@
 			}
 			
 			$view_path = 'views/' . $view . '.phtml';
-			$view_file = APP_ROOT . '/' . $view_path;
 
-			if ( file_exists( $view_file ) ) {
+			if ( self::view_exists( $view ) ) {
 				include_once( $view_path );
 			}
 			elseif ( DEBUG ) {
-				echo "{View doesn't exist: $view_file}";
+				echo "{View doesn't exist: $view_path}";
 			}
+		}
+
+		public static function view_exists( $path ) {
+			return file_exists( APP_ROOT . '/views/' . $path . '.phtml' );
 		}
 	}
